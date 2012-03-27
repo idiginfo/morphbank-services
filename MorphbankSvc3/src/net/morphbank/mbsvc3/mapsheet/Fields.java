ba10cb582ef2efa1cde015917eea1827b5042154
cmonnom <cmonnom@5fd7fc25-0cb3-4b23-852e-83749ec7f503>
2012-03-27 17:03:23 +0000

    Interface to easily modify headers

---
+++ MorphbankSvc3/src/net/morphbank/mbsvc3/mapsheet/Fields.java
@@ -0,0 +1,61 @@
+package net.morphbank.mbsvc3.mapsheet;
+
+public abstract class Fields {
+
+	public abstract String getS_SPECIMEN_DESCRIPTION();
+	public abstract String getS_DETERMINED_BY();
+	public abstract String getS_DATE_DETERMINED();
+	public abstract String getS_COMMENT_BY_DETERMINER();
+	public abstract String getS_FORM();
+	public abstract String getS_COLLECTION_CODE();
+	public abstract String getS_INSTITUTION_CODE();
+	public abstract String getS_CATALOG_NUMBER();
+	public abstract String getS_PREVIOUS_CATALOG_NUMBER();
+	public abstract String getS_RELATED_CATALOG_ITEM();
+	public abstract String getS_RELATIONSHIP_TYPE();
+	public abstract String getS_COLLECTION_NUMBER();
+	public abstract String getS_BASIS_OF_RECORD();
+	public abstract String getS_SEX();
+	public abstract String getS_TYPE_STATUS();
+	public abstract String getS_DEVELOPMENTAL_STAGE();
+	public abstract String getS_PREPARATION_TYPE();
+	public abstract String getS_COLLECTOR_NAME();
+	public abstract String getS_NOTES();
+	public abstract String getS_EARLIEST_DATE_COLLECTED();
+	public abstract String getS_LATEST_DATE_COLLECTED();
+	public abstract String getS_CONTINENT();
+	public abstract String getS_OCEAN();
+	public abstract String getS_WATER_BODY();
+	public abstract String getS_COUNTRY();
+	public abstract String getS_STATE_OR_PROVINCE();
+	public abstract String getS_COUNTY();
+	public abstract String getS_LOCALITY();
+	public abstract String getS_INFORMATION_WITHHELD();
+	public abstract String getS_LATITUDE();
+	public abstract String getS_LONGITUDE();
+	public abstract String getS_MINIMUM_ELEVATION();
+	public abstract String getS_MAXIMUM_ELEVATION();
+	public abstract String getS_MINIMUM_DEPTH();
+	public abstract String getS_MAXIMUM_DEPTH();
+	public abstract String getS_INSTITUTION_NAME();
+	public abstract String getS_LOCALITY_DESCRIPTION();
+
+	public abstract String getI_IMAGE_DESCRIPTION();
+	public abstract String getI_ORIGINAL_FILE_NAME();
+	public abstract String getI_IMAGEURL();
+	public abstract String getI_CREATIVE_COMMONS();
+	public abstract String getI_PHOTOGRAPHER();
+	public abstract String getI_COPYRIGHT();
+	public abstract String getI_ENCYCLOPEDIA_OF_LIFE();
+	
+	public abstract String getV_VIEW_DESCRIPTION();
+	public abstract String getV_VIEW_ANGLE();
+	public abstract String getV_IMAGING_TECHNIQUE();
+	public abstract String getV_IMAGING_PREPARATION_TECHNIQUE();
+	public abstract String getV_VIEW_SEX();
+	public abstract String getV_VIEW_FORM();
+	public abstract String getV_VIEW_DEVELOPMENTAL_STAGE();
+	public abstract String getV_SPECIMEN_PART();
+	public abstract String getV_VIEW_APPLICABLE_TO_TAXON();
+
+}
