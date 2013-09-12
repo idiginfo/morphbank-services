ca38cad09555383a12a1f9cc3b0151a00626ecb5
griccardi <griccardi@5fd7fc25-0cb3-4b23-852e-83749ec7f503>
2013-09-12 15:57:15 +0000

    modified to be maven project

--- trunk/services/MorphbankCore/src/net/morphbank/object/TaxonBranchNodeId.java
+++ trunk/services/MorphbankCore/src/net/morphbank/object/TaxonBranchNodeId.java
@@ -1,9 +1,24 @@
 package net.morphbank.object;
 
-public class TaxonBranchNodeId {
+import java.io.Serializable;
+
+public class TaxonBranchNodeId implements Serializable {
+
+	private static final long serialVersionUID = 1L;
 
 	int child;
 
 	int tsn;
 
+	public boolean equals(Object obj) {
+		if (obj instanceof TaxonBranchNodeId) {
+			TaxonBranchNodeId id = (TaxonBranchNodeId) obj;
+			return child == id.child && tsn == id.tsn;
+		}
+		return false;
+	}
+
+	public int hashCode() {
+		return super.hashCode();
+	}
 }
