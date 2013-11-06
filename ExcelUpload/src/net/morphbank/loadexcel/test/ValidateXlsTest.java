7fc098b27b8a25e7ddcb0de0695f3ff8092d72fe
sgautamfsu <sgautamfsu@5fd7fc25-0cb3-4b23-852e-83749ec7f503>
2013-11-06 15:05:58 +0000

    Modified to use org.apache.poi.ss.usermodel package

--- ExcelUpload/src/net/morphbank/loadexcel/test/ValidateXlsTest.java
+++ ExcelUpload/src/net/morphbank/loadexcel/test/ValidateXlsTest.java
@@ -10,10 +10,27 @@ public class ValidateXlsTest {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		
+		/*
 		SheetReader sheetReader = new SheetReader("/home/gjimenez/palearticTestmb3a-1.xls", null);
 //		SheetReader sheetReader = new SheetReader("/home/gjimenez/Morphbank/uploads/Elijah/African_Paridris_mb3a_DPand ET_GJ.xls", null);
 		ValidateXls test = new ValidateXls(sheetReader, true, "loadData.properties");
 		System.out.println(test.checkEverything());
+		*/
+		 
+		
+		SheetReader sheetReader = new SheetReader("C:\\sg11x\\workbooks\\verifyExcelUpload\\0MORPHBANK_salgueiro_theridiids_DP24Oct2013.xls", null);
+		//SheetReader sheetReader = new SheetReader("C:\\sg11x\\workbooks\\verifyExcelUpload\\with Errors\\MB_salgueiro_theridiids_WithErrors.xls", null);
+		//SheetReader sheetReader = new SheetReader("C:\\sg11x\\workbooks\\verifyExcelUpload\\with Errors\\0MORPHBANK_SAL_CORI_ORIZABA-Orig.xls", null);
+		
+		ValidateXls test = new ValidateXls(sheetReader, true, "loadData.properties");
+		boolean testResult = test.checkEverything();
+		if(testResult){
+			System.out.println("Test Passed !");
+		}
+		else{
+			System.out.println("Test Failed !");
+		}
 	}
 
 }
