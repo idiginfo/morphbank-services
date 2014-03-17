c6faff8b800a21661e89b3c8c57adfd58f59eb55
griccardi <griccardi@5fd7fc25-0cb3-4b23-852e-83749ec7f503>
2014-03-17 19:21:58 +0000

    service3_0: experimenting with UsageExample for dwca

--- trunk/services/service3_0/src/main/java/net/morphbank/mbsvc3/mapdwca/UsageExample.java
+++ trunk/services/service3_0/src/main/java/net/morphbank/mbsvc3/mapdwca/UsageExample.java
@@ -4,9 +4,14 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import org.apache.log4j.PropertyConfigurator;
+import org.apache.log4j.BasicConfigurator;
 
 import org.gbif.dwc.record.DarwinCoreRecord;
 import org.gbif.dwc.record.Record;
+import org.gbif.dwc.terms.ConceptTerm;
 import org.gbif.dwc.terms.DwcTerm;
 import org.gbif.dwc.text.*;
 
@@ -17,15 +22,31 @@ public class UsageExample {
 
 	public static void main(String[] args) throws IOException,
 			UnsupportedArchiveException {
+		
+		//BasicConfigurator.configure();
+		//PropertyConfigurator.configure();
+		
 		// opens csv files with headers or dwc-a direcotries with a meta.xml
 		// descriptor
 		Archive arch = ArchiveFactory.openArchive(new File(INPUT_FILE));
+		
 
 		// does scientific name exist?
 		if (!arch.getCore().hasTerm(DwcTerm.scientificName)) {
 			System.out
 					.println("This application requires dwc-a with scientific names");
 			System.exit(1);
+		}
+		
+		Set<ArchiveFile> extensions = arch.getExtensions();
+		String t = null;
+		
+		
+		for (ArchiveFile extension: extensions){
+			ArchiveField id = extension.getId();
+			Map<ConceptTerm, ArchiveField> fields = extension.getFields();
+			t = extension.getRowType();
+			System.out.println("extension has row type: "+t);
 		}
 
 		// loop over core darwin core records
